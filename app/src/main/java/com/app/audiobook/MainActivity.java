6e17f57eb01e0c1a9a39ac5c765f207d10097011
Dromikys <llKiNGll280900>
2020-06-23 14:10:19 +0300

    start project

---
+++ app/src/main/java/com/app/audiobook/MainActivity.java
@@ -0,0 +1,34 @@
+package com.app.audiobook;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.app.audiobook.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        viewPager.setCurrentItem(2);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+}
